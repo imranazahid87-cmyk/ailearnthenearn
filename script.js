bb24170027e36af80dc016e09f1d50878ff87c44
imranazahid87-cmyk <imranazahid87@gmail.com>
2026-07-19 12:02:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,23 @@ function toggleMenu(){
 document.querySelector("nav").classList.toggle("show");
 
 }
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
+topBtn.style.display="block";
+}else{
+topBtn.style.display="none";
+}
+
+};
+
+function topFunction(){
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+}
