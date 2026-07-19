e549281aa432e5aec8ae63adc9f5c4dca20cd381
imranazahid87-cmyk <imranazahid87@gmail.com>
2026-07-19 11:38:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 
+function searchWebsite(){
+
+let input=document.querySelector(".search-box input").value.toLowerCase();
+
+if(input.includes("chatgpt")){
+window.location.href="tools.html";
+}
+
+else if(input.includes("gemini")){
+window.location.href="tools.html";
+}
+
+else if(input.includes("seo")){
+window.location.href="courses.html";
+}
+
+else if(input.includes("course")){
+window.location.href="courses.html";
+}
+
+else if(input.includes("blog")){
+window.location.href="blog.html";
+}
+
+else if(input.includes("earn")){
+window.location.href="earn.html";
+}
+
+else{
+
+alert("No result found.");
+
+}
+
+}
